c3c354ee6347b6d7b0908f32c1961eed81aeef9c
martin <goudingcheng@gmail.com>
2025-10-25 10:08:00 +0800

    feature:update the apache licence

--- src/main/java/com/github/paohaijiao/handler/AbsConnectorBaseHandler.java
+++ src/main/java/com/github/paohaijiao/handler/AbsConnectorBaseHandler.java
@@ -6,8 +6,15 @@ import com.github.paohaijiao.data.Row;
 import java.util.List;
 
 public abstract class AbsConnectorBaseHandler implements ConnectorHandler {
+    /**
+     * buildColumns
+     * @return
+     */
+    public abstract List<ColumnMeta> buildColumns();
 
-    public abstract List<ColumnMeta> buildColumnMetaList();
-
+    /**
+     * buildRow
+     * @return
+     */
     public abstract List<Row> buildRow();
 }
--- src/main/java/com/github/paohaijiao/handler/AbsDatabaseConnectorBaseHandler.java
+++ src/main/java/com/github/paohaijiao/handler/AbsDatabaseConnectorBaseHandler.java
@@ -1,12 +1,73 @@
 package com.github.paohaijiao.handler;
 
-import com.github.paohaijiao.config.ConnectorConfig;
-import com.github.paohaijiao.connection.JSqlConnectionFactory;
-import com.github.paohaijiao.data.ColumnMeta;
-import com.github.paohaijiao.data.Row;
 
-import java.util.List;
+import com.github.paohaijiao.config.ConnectorBaseConfig;
+import com.github.paohaijiao.connection.JSqlConnectionFactory;
+import com.github.paohaijiao.data.DataSet;
+import com.github.paohaijiao.data.Row;
+import com.github.paohaijiao.exception.JAssert;
+
+import java.sql.*;
 
 public abstract class AbsDatabaseConnectorBaseHandler extends AbsConnectorBaseHandler {
-   // public JSqlConnectionFactory doConnection(ConnectorConfig config);
+
+    private static final  String driver="driver";
+
+    private static final  String url="url";
+
+    private static final  String username="username";
+
+    private static final  String password="password";
+    private static final  String sql="sql";
+    /**
+     *
+     * @param config
+     * @return
+     */
+    public JSqlConnectionFactory doConnection(ConnectorBaseConfig config){
+        String connectorDriverClass =config.getProperty(driver,String.class);
+        JAssert.notEmptyStr(connectorDriverClass,"driver class require not null");
+        String connectorUrl=config.getProperty(url,String.class);
+        JAssert.notEmptyStr(connectorUrl,"url  require not null");
+        String connectorUsername=config.getProperty(username,String.class);
+        JAssert.notEmptyStr(connectorUsername,"username  require not null");
+        String connectorPassword=config.getProperty(password,String.class);
+        JAssert.notEmptyStr(connectorPassword,"password  require not null");
+        String connectorSql=config.getProperty(sql,String.class);
+        JAssert.notEmptyStr(connectorSql,"sql  require not null");
+        try {
+            Class.forName(connectorDriverClass);
+            Connection connection = DriverManager.getConnection(url, username, password);
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            DataSet dataSet = doDataSet(rs);
+            rs.close();
+            pstmt.close();
+            connection.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+    private DataSet doDataSet(ResultSet rs) throws SQLException {
+        DataSet.Builder builder = DataSet.builder();
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        for (int i = 1; i <= columnCount; i++) {
+            String columnName = metaData.getColumnName(i);
+            String source = metaData.getTableName(i);
+//            Class<?> columnType = getJavaType(metaData.getColumnType(i));
+//            builder.addColumn(columnName, columnType, source);
+        }
+        while (rs.next()) {
+            Row row = new Row();
+            for (int i = 1; i <= columnCount; i++) {
+                String columnName = metaData.getColumnName(i);
+                Object value = rs.getObject(i);
+                row.put(columnName, value);
+            }
+            builder.addRow(row);
+        }
+        return builder.build();
+    }
 }
